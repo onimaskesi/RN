559f67e18cfa9b5fcdd8b4969e19e9d3b1904390
onimaskesi <onimaskesi@gmail.com>
2021-08-10 16:33:39 +0300

    fontStyle

--- FontFamilyAdd/FontFamilyDemo1/App.js
+++ FontFamilyAdd/FontFamilyDemo1/App.js
@@ -6,16 +6,36 @@ export default () => {
     <SafeAreaView style={styles.container}>
       <Text style={styles.textBold}>Gilroy-ExtraBold</Text>
       <Text style={styles.textLight}>Gilroy-Light</Text>
+      <Text style={styles.textMosaic}>PaletteMosaic-Regular</Text>
     </SafeAreaView>
   );
 };
 
+const baseStyle = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    margin: 10,
+  },
+});
+
+const {text: baseTextStyle} = baseStyle;
+
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   textBold: {
+    ...baseTextStyle,
     fontFamily: 'Gilroy-ExtraBold',
   },
   textLight: {
+    ...baseTextStyle,
     fontFamily: 'Gilroy-Light',
+  },
+  textMosaic: {
+    ...baseTextStyle,
+    fontFamily: 'PaletteMosaic-Regular',
   },
 });
